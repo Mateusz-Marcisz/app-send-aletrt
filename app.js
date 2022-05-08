d8b4ed6d77e531acc283793f106bfc7ba4508603
Mateusz-Marcisz <72744874+Mateusz-Marcisz@users.noreply.github.com>
2022-05-08 03:26:19 +0200

    m

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const Nexmo = require('nexmo');
+const socketio = require('socket.io');
+const text = require('body-parser/lib/types/text');
+
+// Init Nexmo
+const nexmo = new Nexmo({
+  apiKey: 'YOURAPIKEY',
+  apiSecret: 'YOURAPISECRET'
+}, { debug: true });
+
+// Init app
+const app = express();
+
+// Template engine setup
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+
+// Public folder setup
+app.use(express.static(__dirname + '/public'));
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Index route
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// Catch form submit
+app.post('/', (req, res) => {
+  // res.send(req.body);
+  // console.log(req.body);
+  const { number, text } = req.body;
+
+console.log(text);
+});
+
+// Define port
+const port = 3000;
+
+// Start server
+const server = app.listen(port, () => console.log(`Server started on port ${port}`));
+
+// Connect to socket.io
+const io = socketio(server);
+io.on('connection', (socket) => {
+  console.log('Connected');
+  io.on('disconnect', () => {
+    console.log('Disconnected');
+  })
+});
